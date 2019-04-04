390ef320a25048e9522c58c3bf6c31fcdf3befa0
tomoki.hosotani@border.co.jp <tomoki.hosotani@border.co.jp>
2019-04-04 14:28:22 +0900

    Add user microposts

---
+++ app/models/micropost.rb
@@ -0,0 +1,19 @@
+class Micropost < ApplicationRecord
+  belongs_to :user
+  default_scope -> { order(created_at: :desc) }
+  mount_uploader :picture, PictureUploader
+  validates :user_id, presence: true
+  validates :content, presence: true, length: { maximum: 140 }
+  validates :score, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1,  less_than_or_equal_to: 5}
+  validate  :picture_size
+
+  private
+
+    # アップロードされた画像のサイズをバリデーションする
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add(:picture, "should be less than 5MB")
+      end
+    end
+    
+end
